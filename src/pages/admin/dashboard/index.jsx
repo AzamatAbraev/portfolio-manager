41e9834dea5bab684ce039faaed8b03ba52f1f91
Your Name <azamatabraev07@gmail.com>
2023-11-01 14:49:24 +0500

    loading added in public pages

--- src/pages/admin/dashboard/index.jsx
+++ src/pages/admin/dashboard/index.jsx
@@ -1,43 +1,47 @@
+import { Spin } from "antd";
 import { useGetExperiencesQuery } from "../../../redux/queries/experience";
 import { useGetMessagesQuery } from "../../../redux/queries/messages";
 import { useGetPortfoliosQuery } from "../../../redux/queries/portfolio";
 import { useGetUsersQuery } from "../../../redux/queries/users";
+
 import "./style.scss";
 
 const DashboardPage = () => {
-  const {data: messages} = useGetMessagesQuery();
+  const { data: messages, isFetching } = useGetMessagesQuery();
   const { data: users } = useGetUsersQuery();
   const { data: portfolios } = useGetPortfoliosQuery();
   const { data: experience } = useGetExperiencesQuery();
   return (
-    <section>
-      <div className="container">
-        <div className="main-stats">
-          <h2 className="dashboard-title">Welcome abord, admin</h2>
-          <p className="dashboard-desc">
-            Stay updated with the latest statistics
-          </p>
+    <Spin spinning={isFetching}>
+      <section>
+        <div className="container">
+          <div className="main-stats">
+            <h2 className="dashboard-title">Welcome abord, admin</h2>
+            <p className="dashboard-desc">
+              Stay updated with the latest statistics
+            </p>
+          </div>
+          <div className="stats-row">
+            <div className="stats-card">
+              <h3>Active users</h3>
+              <p>+{users?.total}</p>
+            </div>
+            <div className="stats-card">
+              <h3>Experienced users</h3>
+              <p>+{experience?.total}</p>
+            </div>
+            <div className="stats-card">
+              <h3>Messages</h3>
+              <p>+{messages?.total}</p>
+            </div>
+            <div className="stats-card">
+              <h3>Portfolios</h3>
+              <p>+{portfolios?.total}</p>
+            </div>
+          </div>
         </div>
-        <div className="stats-row">
-          <div className="stats-card">
-            <h3>Active users</h3>
-            <p>+{users?.total}</p>
-          </div>
-          <div className="stats-card">
-            <h3>Experienced users</h3>
-            <p>+{experience?.total}</p>
-          </div>
-          <div className="stats-card">
-            <h3>Messages</h3>
-            <p>+{messages?.total}</p>
-          </div>
-          <div className="stats-card">
-            <h3>Portfolios</h3>
-            <p>+{portfolios?.total}</p>
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </Spin>
   );
 };
 
--- src/pages/public/login/index.jsx
+++ src/pages/public/login/index.jsx
@@ -6,10 +6,26 @@ import { TOKEN, USER } from "../../../constants";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setAuth } from "../../../redux/slices/auth";
+import { Fragment, useEffect, useState } from "react";
+import Loader from "../../../components/loader/Loader";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    let timerId = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, []);
+
   const login = async (values) => {
     try {
       const {
@@ -18,81 +34,90 @@ const LoginPage = () => {
       Cookies.set(TOKEN, token);
       localStorage.setItem(USER, JSON.stringify(user));
       dispatch(setAuth(user));
+      setIsLoading(true)
       if (user.role === "admin") {
         navigate("/dashboard");
       } else {
-        navigate("/")
+        navigate("/");
       }
     } catch (error) {
       message.error(error.response.data.message);
+    } finally {
+      setIsLoading(false)
     }
   };
   return (
-    <section className="login">
-      <div className="container">
-        <h2 className="login__title">Sign in</h2>
-        <Form
-          className="login-form"
-          name="login"
-          labelCol={{
-            span: 24,
-          }}
-          wrapperCol={{
-            span: 24,
-          }}
-          onFinish={login}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: "Please input your username!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
+    <Fragment>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section className="login">
+          <div className="container">
+            <h2 className="login__title">Sign in</h2>
+            <Form
+              className="login-form"
+              name="login"
+              labelCol={{
+                span: 24,
+              }}
+              wrapperCol={{
+                span: 24,
+              }}
+              onFinish={login}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your username!",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: "Please input your password!",
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your password!",
+                  },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
 
-          <Form.Item
-            className="btn-container"
-            wrapperCol={{
-              offset: 0,
-              span: 24,
-            }}
-          >
-            <button className="submit-btn" type="submit">
-              Login
-            </button>
-          </Form.Item>
-          <div>
-            <div className="login__subinfo">
-              <p>Do not have an account yet? </p>
-              <Link to="/register">Register here</Link>
-            </div>
-            <div className="login__subinfo">
-              <p>Not ready yet ?</p>
-              <Link to="/">See our home page</Link>
-            </div>
+              <Form.Item
+                className="btn-container"
+                wrapperCol={{
+                  offset: 0,
+                  span: 24,
+                }}
+              >
+                <button className="submit-btn" type="submit">
+                  Login
+                </button>
+              </Form.Item>
+              <div>
+                <div className="login__subinfo">
+                  <p>Do not have an account yet? </p>
+                  <Link to="/register">Register here</Link>
+                </div>
+                <div className="login__subinfo">
+                  <p>Not ready yet ?</p>
+                  <Link to="/">See our home page</Link>
+                </div>
+              </div>
+            </Form>
           </div>
-        </Form>
-      </div>
-    </section>
+        </section>
+      )}
+    </Fragment>
   );
 };
 
